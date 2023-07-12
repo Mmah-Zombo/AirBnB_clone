24c5378baa8bd7f474a2c9319e175492b0aab8cf
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-12 18:01:28 +0000

    corrected the unittest for the save method

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -11,13 +11,14 @@ from models.base_model import BaseModel
 
 class TestBaseModel_Save(unittest.TestCase):
     """unittests for the save public method"""
-    def test_one_save(self):
+    def test_save_one(self):
         """tests if the save method works for a normal case"""
         obj = BaseModel()
         sleep(1)
         first_update_time = obj.updated_at
         obj.save()
         self.assertLess(first_update_time, obj.updated_at)
+
 
 if __name__ == "__main__":
     unittest.main()
