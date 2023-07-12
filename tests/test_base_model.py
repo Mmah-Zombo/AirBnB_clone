f3b64e281f3fb70b40c70e5d786c1ad4fda7cae2
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-12 17:42:18 +0000

    adds unittest for the save method of BaseModel class

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""Unnittests for models/base_model.py"""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseModel_Save(unittest.TestCase):
+    """unittests for the save public method"""
+    def test_one_save(self):
+        """tests if the save method works for a normal case"""
+        obj = BaseModel()
+        sleep(1)
+        first_update_time = obj.updated_at
+        obj.save()
+        self.assertLess(first_update_time, obj.updated_at)
+
+if __name__ == "__main__":
+    unittest.main()
