55cce23ae0d5a5ab489f5beedf039ffd4de71cb3
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-13 12:36:19 +0000

    unittests properly added

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
-"""Unnittests for models/base_model.py"""
-
-import os
-import models
+"""Unittests for the BaseModel"""
 import unittest
 from datetime import datetime
-from time import sleep
 from models.base_model import BaseModel
 
-
-class TestBaseModel_Save(unittest.TestCase):
-    """unittests for the save public method"""
-    def test_save_one(self):
-        """tests if the save method works for a normal case"""
+class TestBaseModel(unittest.TestCase):
+    """Unittesting for each method and attribute of the BaseModel"""
+    def test_id_is_string(self):
         obj = BaseModel()
-        sleep(0.05)
-        first_update_time = obj.updated_at
+        self.assertIsInstance(obj.id, str)
+
+    def test_created_at_is_datetime(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.created_at, datetime)
+
+    def test_updated_at_is_datetime(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.updated_at, datetime)
+
+    def test_str_representation(self):
+        obj = BaseModel()
+        obj_str = str(obj)
+        class_name = obj.__class__.__name__
+        expected_str = f"[{class_name}] ({obj.id}) {obj.__dict__}"
+        self.assertEqual(obj_str, expected_str)
+
+    def test_save_updates_updated_at(self):
+        obj = BaseModel()
+        original_updated_at = obj.updated_at
         obj.save()
-        self.assertLess(first_update_time, obj.updated_at)
+        self.assertNotEqual(obj.updated_at, original_updated_at)
+
+    def test_to_dict_returns_dict_representation(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
