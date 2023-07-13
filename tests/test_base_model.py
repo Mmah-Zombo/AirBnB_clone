2e5e59458b0d7d7076f7496e0e6a96aaae375895
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-13 12:40:46 +0000

    added tests for specific methods and attributes

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -6,25 +6,6 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Unittesting for each method and attribute of the BaseModel"""
-    def test_id_is_string(self):
-        obj = BaseModel()
-        self.assertIsInstance(obj.id, str)
-
-    def test_created_at_is_datetime(self):
-        obj = BaseModel()
-        self.assertIsInstance(obj.created_at, datetime)
-
-    def test_updated_at_is_datetime(self):
-        obj = BaseModel()
-        self.assertIsInstance(obj.updated_at, datetime)
-
-    def test_str_representation(self):
-        obj = BaseModel()
-        obj_str = str(obj)
-        class_name = obj.__class__.__name__
-        expected_str = f"[{class_name}] ({obj.id}) {obj.__dict__}"
-        self.assertEqual(obj_str, expected_str)
-
     def test_save_updates_updated_at(self):
         obj = BaseModel()
         original_updated_at = obj.updated_at
@@ -38,6 +19,21 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(obj_dict['__class__'], 'BaseModel')
         self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
         self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
+
+    def test_id_is_string(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.id, str)
+
+    def test_created_at_is_datetime(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.created_at, datetime)
+
+    def test_str_representation(self):
+        obj = BaseModel()
+        obj_str = str(obj)
+        class_name = obj.__class__.__name__
+        expected_str = f"[{class_name}] ({obj.id}) {obj.__dict__}"
+        self.assertEqual(obj_str, expected_str)
 
 
 if __name__ == '__main__':
