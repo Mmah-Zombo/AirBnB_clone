193f15d1dc1560927d65bca7179d2f7459d9ba91
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-19 19:49:49 +0000

    console.py again

--- console.py
+++ console.py
@@ -9,14 +9,23 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
-    def quit(self, arg):
-        """quit command to exit the program"""
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
         return True
 
-    def EOF(self, arg):
+    def do_EOF(self, arg):
         """Exit the command interpreter on EOF (Ctrl+D)."""
         print()
         return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+    def do_save(self, arg):
+        """Save command to save data."""
+        # Implement your save logic here
+        pass
 
     def emptyline(self):
         """Do nothing when an empty line is entered."""
