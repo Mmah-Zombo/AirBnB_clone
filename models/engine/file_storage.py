6660d1e76c738b13458e4fdd987c8d57c42590c3
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-19 18:13:53 +0000

    redo the entire storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
 """Contains the file storage class"""
 import json
-import os
-from models.base_model import BaseModel
 
 
 class FileStorage:
-    """the file storage class
-
-    Attributes:
-    __file_path (str): the path to the file and its name
-    __objects (str): a dictionary of instatiated objects.
-    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        obj_class_name = obj.__class_.__name__
-        k = "{}.{}".format(obj_class_name, obj.id)
-        self.__objects[k] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file"""
+        """Serializes __objects to the JSON file (path: __file_path)"""
         data = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             data[key] = obj.to_dict()
-
-        with open("file.json", "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(data, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
-        if (os.path.isfile(FileStorage.__file_path)):
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
-                l_json = json.load(fname)
-                for key, val in l_json.items():
-                    FileStorage.__objects[key] = eval(
-                        val['__class__'])(**val)
+        try:
+            with open(FileStorage.__file_path, "r") as file:
+                data = json.load(file)
+                from models.base_model import BaseModel
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    class_ = BaseModel if class_name == "BaseModel" else None
+                    if class_:
+                        obj = class_(**value)
+                        FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
