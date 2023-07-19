3a2fb96fe73e7c044552ff0db8bb07c6e550401f
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-19 18:37:20 +0000

    redo the console.py file

--- console.py
+++ console.py
@@ -1,41 +1,50 @@
 #!/usr/bin/python3
-"""contains the entry point of the command interpreter"""
+"""Contains the entry point of the command interpreter."""
+
 import cmd
-from models.engine.file_storage import FileStorage
-from models import storage
-from datetime import datetime
 
 
 class HBNBCommand(cmd.Cmd):
-    """the console object"""
-    prompt = "(hbnb) "
+    """Command interpreter class."""
 
-    def emptyline(self):
-        """Called when an empty line is entered. Does nothing."""
-        pass
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Exit the program.
-        Usage: quit
-        """
+        """Exit the command interpreter."""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program using EOF (Ctrl+D).
-        """
-        print()
+        """Exit the command interpreter on EOF (Ctrl+D)."""
         return True
 
-    def do_help(self, arg):
-        """List available commands with "help"
-        or detailed help with "help cmd".
-        Usage: help [cmd]
-        """
-        cmd.Cmd.do_help(self, arg)
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
 
-    # Define additional commands here (e.g., create, show, update, destroy)
+    def help_quit(self):
+        """Display help message for the quit command."""
+        print("Quit command to exit the command interpreter.")
+
+    def help_EOF(self):
+        """Display help message for the EOF (Ctrl+D) command."""
+        print("Exit the command interpreter on EOF (Ctrl+D).")
+
+    def help_help(self):
+        """Display help message for the help command."""
+        print("Show help message for the specified \
+              command or list available commands.")
+
+    def help_create(self):
+        """Display help message for the create command."""
+        print("Create a new instance of BaseModel, save it, and print its id.")
+
+    def help_show(self):
+        """Display help message for the show command."""
+        print("Show the string representation of an instance \
+              based on the class name and id.")
+
+    # Add other command help methods as needed
 
 
-if __name__ == "__main__":
-    # Start the command interpreter
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
